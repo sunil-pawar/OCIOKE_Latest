686576f9cb0b2b047d322cc90ab38e9d133498c5
Sunil Rajashekar Pawar <SR00461257@techmahindra.com>
2021-07-25 09:54:54 -0300

    OCIOKE  working without prometheus

---
+++ datasources.tf
@@ -0,0 +1,11 @@
+# Gets a list of Availability Domains
+data "oci_identity_availability_domains" "ADs" {
+  compartment_id = "${var.compartment_ocid}"
+}
+ 
+# https://www.terraform.io/docs/providers/oci/d/core_images.html
+data "oci_core_images" "oracle_linux_image" {
+  compartment_id           = "${var.compartment_ocid}"
+  operating_system         = "Oracle Linux"
+  operating_system_version = "7.6"
+}
